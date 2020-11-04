6dd0725ca96013af25ee4a4de1e93dd18a591db2
Nelson Foltz <nelsfoltz@gmail.com>
2020-11-04 14:20:41 -0500

    add: focusing input after clearing with useRef

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 export default function App() {
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState('react hooks');
+  const searchInputRef = useRef();
 
   useEffect(() => {
     getResults();
@@ -21,6 +22,11 @@ export default function App() {
     getResults();
   };
 
+  const handleClearSearch = () => {
+    setQuery('');
+    searchInputRef.current.focus();
+  };
+
   return (
     <>
       <form onSubmit={handleSearch}>
@@ -28,8 +34,12 @@ export default function App() {
           type="text"
           onChange={event => setQuery(event.target.value)}
           value={query}
+          ref={searchInputRef}
         />
         <button type="submit">search</button>
+        <button type="button" onClick={handleClearSearch}>
+          clear
+        </button>
         <ul>
           {results.map(result => (
             <li key={result.objectID}>
