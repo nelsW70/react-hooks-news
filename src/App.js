38cec029bfd6e9a37ee91a9a9a6e34cec2b974aa
Nelson Foltz <nelsfoltz@gmail.com>
2020-11-04 15:21:21 -0500

    add: styling

--- src/App.js
+++ src/App.js
@@ -37,32 +37,48 @@ export default function App() {
   };
 
   return (
-    <>
-      <form onSubmit={handleSearch}>
+    <div className="container max-w-md mx-auto p-4 m-2 bg-gray-100 shadow-lg rounded">
+      <h1 className="text-grey-darkest font-thin">Hooks News</h1>
+      <form onSubmit={handleSearch} className="mb-2">
         <input
           type="text"
           onChange={event => setQuery(event.target.value)}
           value={query}
           ref={searchInputRef}
+          className="border p-2 rounded"
         />
-        <button type="submit">search</button>
-        <button type="button" onClick={handleClearSearch}>
+        <button
+          type="submit"
+          className="bg-orange-500 text-white rounded m-1 p-2"
+        >
+          search
+        </button>
+        <button
+          type="button"
+          onClick={handleClearSearch}
+          className="bg-teal-500 text-white p-2 rounded"
+        >
           clear
         </button>
       </form>
 
       {loading ? (
-        <div>Loading results...</div>
+        <div className="font-bold text-black-900">Loading results...</div>
       ) : (
-        <ul>
+        <ul className="list-reset leading-normal">
           {results.map(result => (
             <li key={result.objectID}>
-              <a href={result.url}>{result.title}</a>
+              <a
+                href={result.url}
+                className="text-indigo-700 hover:text-indigo-900"
+              >
+                {result.title}
+              </a>
             </li>
           ))}
         </ul>
       )}
-      {error && <div>{error.message}</div>}
-    </>
+      {error && <div className="text-red font-bold">{error.message}</div>}
+    </div>
   );
 }
