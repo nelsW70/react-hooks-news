8f4de7478f27b29084bb35fca138cf2831dbbe5d
Nelson Foltz <nelsfoltz@gmail.com>
2020-11-04 10:19:06 -0500

    add: fetching data on component mount with useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+      .then(response => {
+        console.log(response.data);
+        setResults(response.data.hits);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {results.map(result => (
+          <li key={result.objectID}>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
-
-export default App;
